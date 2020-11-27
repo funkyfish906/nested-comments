91e2b837a476cf1590d7b4d8ac21964144fd4364
funkyfish906 <funkyfish906@gmail.com>
2020-11-27 12:55:44 +0200

    fixes

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,7 +4,6 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
         <title>{!! Config::get('app.name') !!}</title>
         <link href="https://fonts.googleapis.com/css?family=Roboto:400,300,100,500,700,900" rel="stylesheet" type="text/css">
         <link rel="stylesheet" href="{{ URL::asset('css/bootstrap.min.css') }}">
